7dcfbcd03e6f783bc3526e0500a8cb99c82525d8
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-03-04 13:54:17 +0300

    fix: RST-27 Fix code-ordering widget template

--- src/ui/widgets/code-ordering.widget/code-ordering.widget.ts
+++ src/ui/widgets/code-ordering.widget/code-ordering.widget.ts
@@ -1,9 +1,21 @@
-import { HEADINGS_TWO } from "../../../constants.js";
+import {
+  CLASS_NAME,
+  EVENT,
+  HEADINGS_THREE,
+  HEADINGS_TWO,
+} from "../../../constants.js";
+import { CodeOrderingPayload } from "../../../interfaces/widget-payload.interfaces.js";
+import { CodeOrderingAnswer } from "../../../interfaces/widget-user-answer.interfaces.js";
+import ButtonCreator from "../../../utils/button/button-creator.js";
 import ElementCreator from "../../../utils/element-creator.js";
 import HeadingsCreator from "../../../utils/headings/headings-creator.js";
+import ParagraphCreator from "../../../utils/paragraph/paragraph-creator.js";
 import "./code-ordering.widget.css";
 
-export default function codeOrderingWidget(): HTMLElement {
+export default function codeOrderingWidget(
+  payload: CodeOrderingPayload,
+  onAnswer: (answer: CodeOrderingAnswer) => void,
+): HTMLElement {
   const codeOrderingWidgetContainer = new ElementCreator({
     classes: ["code-ordering-widget-container"],
   }).getElement();
@@ -12,6 +24,35 @@ export default function codeOrderingWidget(): HTMLElement {
     parent: codeOrderingWidgetContainer,
     text: "Code Ordering Widget",
   }).getElement();
+
+  new HeadingsCreator(HEADINGS_THREE, {
+    parent: codeOrderingWidgetContainer,
+    text: payload.title,
+  }).getElement();
+
+  new ParagraphCreator({
+    parent: codeOrderingWidgetContainer,
+    text: payload.description,
+  }).getElement();
+
+  new ParagraphCreator({
+    parent: codeOrderingWidgetContainer,
+    text: payload.lines.join(""),
+  }).getElement();
+
+  const submitButton = new ButtonCreator({
+    text: "Submit",
+    classes: [CLASS_NAME.button],
+    parent: codeOrderingWidgetContainer,
+  }).getElement();
+
+  const selectedAnswerIndex: CodeOrderingAnswer = {
+    answer: [0, 1, 2, 3, 4, 5, 6],
+  };
+
+  submitButton.addEventListener(EVENT.click, () => {
+    onAnswer(selectedAnswerIndex);
+  });
 
   return codeOrderingWidgetContainer;
 }
