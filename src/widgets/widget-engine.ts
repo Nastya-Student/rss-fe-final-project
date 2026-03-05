986e6285a29c525d5ca4aeda9457d290e6febb7e
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-03-05 21:57:26 +0300

    feat: RST-26 Add widget engine session control

--- src/widgets/widget-engine.ts
+++ src/widgets/widget-engine.ts
@@ -1,6 +1,10 @@
+import { CLASS_NAME, EVENT } from "../constants.js";
 import { WidgetStrategy } from "../interfaces/widget-strategy.interface.js";
+import { RoutePath } from "../types/route-path.enum.js";
 import { WidgetType } from "../types/widget-type.type.js";
-import { WidgetAnswerMap, WidgetMap } from "../types/widget.type.js";
+import { Widget, WidgetAnswerMap, WidgetMap } from "../types/widget.type.js";
+import ButtonCreator from "../utils/button/button-creator.js";
+import ElementCreator from "../utils/element-creator.js";
 import ParagraphCreator from "../utils/paragraph/paragraph-creator.js";
 import { asyncSorterStrategy } from "./async-sorter.widget/async-sorter.strategy.js";
 import { codeCompletionStrategy } from "./code-completion.widget/code-completion.strategy.js";
@@ -23,21 +27,28 @@ const widgetStrategies: {
 };
 
 export class WidgetEngine {
+  private widgets: Widget[];
+  private currentIndex = 0;
   private container;
+  private widgetContainer: HTMLElement;
 
-  constructor(container: HTMLElement) {
+  constructor(widgets: Widget[], container: HTMLElement) {
+    this.widgets = widgets;
     this.container = container;
+    this.widgetContainer = new ElementCreator({
+      parent: this.container,
+    }).getElement();
   }
 
   render<T extends WidgetType>(widget: WidgetMap[T] & { type: T }) {
     const strategy = widgetStrategies[widget.type];
 
     this.container.innerHTML = "";
-    this.container.append(
-      strategy.render(widget, (answer) =>
-        this.handleAnswer(widget, strategy, answer),
-      ),
+    this.widgetContainer.innerHTML = "";
+    this.widgetContainer = strategy.render(widget, (answer) =>
+      this.handleAnswer(widget, strategy, answer),
     );
+    this.container.append(this.widgetContainer);
   }
 
   private handleAnswer<T extends WidgetType>(
@@ -45,6 +56,9 @@ export class WidgetEngine {
     strategy: WidgetStrategy<WidgetMap[T], WidgetAnswerMap[T]>,
     answer: WidgetAnswerMap[T],
   ) {
+    const correct = strategy.validate(widget, answer);
+
+    this.showResult(correct);
     new ParagraphCreator({
       parent: this.container,
       text: `Answer: ${JSON.stringify(answer)}`,
@@ -53,5 +67,65 @@ export class WidgetEngine {
       parent: this.container,
       text: `Correct: ${strategy.validate(widget, answer)}`,
     });
+  }
+
+  private showResult(correct: boolean) {
+    new ParagraphCreator({
+      parent: this.widgetContainer,
+      text: correct ? "Correct" : "Wrong",
+    }).getElement();
+
+    const nextButton = new ButtonCreator({
+      parent: this.widgetContainer,
+      text: "Next",
+      classes: [CLASS_NAME.button],
+    }).getElement();
+
+    if (this.currentIndex >= this.widgets.length - 1) {
+      nextButton.textContent = "Go to Results";
+    }
+
+    nextButton.addEventListener(EVENT.click, () => {
+      this.currentIndex += 1;
+
+      if (this.currentIndex >= this.widgets.length) {
+        this.renderResultsScreen();
+        return;
+      }
+
+      this.renderCurrentWidget();
+    });
+  }
+
+  startSession() {
+    this.renderCurrentWidget();
+  }
+
+  private renderCurrentWidget() {
+    const widget = this.widgets[this.currentIndex];
+    if (widget) {
+      this.render(widget);
+    }
+  }
+
+  private renderResultsScreen() {
+    this.container.innerHTML = "";
+    this.widgetContainer.innerHTML = "";
+    const resultsScreenContainer = new ElementCreator({
+      parent: this.container,
+      classes: ["results-screen-container"],
+    }).getElement();
+
+    new ParagraphCreator({
+      parent: resultsScreenContainer,
+      text: "Results Screen",
+    }).getElement();
+
+    const libraryButton = new ButtonCreator({
+      parent: resultsScreenContainer,
+      text: "Go back to Library",
+      classes: [CLASS_NAME.button],
+    }).getElement();
+    libraryButton.dataset.route = RoutePath.Library;
   }
 }
