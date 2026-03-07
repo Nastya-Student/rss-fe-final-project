8a294569e3ef1a64c827a7e18cc7606b3e24c9ff
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-03-07 15:38:14 +0300

    feat: RST-26 Add open widgets by topic on practice page

--- src/pages/practice.page/practice.page.ts
+++ src/pages/practice.page/practice.page.ts
@@ -1,7 +1,10 @@
-import { HEADINGS_TWO } from "../../constants.js";
+import { EVENT, HEADINGS_TWO } from "../../constants.js";
+import { widgetService } from "../../services/widget.service.js";
 import { RoutePath } from "../../types/route-path.enum.js";
 import ButtonCreator from "../../utils/button/button-creator.js";
 import HeadingsCreator from "../../utils/headings/headings-creator.js";
+import shuffleArray from "../../utils/shuffle-array.js";
+import { WidgetEngine } from "../../widgets/widget-engine.js";
 import { BasePage } from "../base-page.js";
 
 export class PracticePage extends BasePage {
@@ -20,5 +23,31 @@ export class PracticePage extends BasePage {
       parent: this.container,
     }).getElement();
     button.dataset.route = RoutePath.Library;
+
+    window.addEventListener(EVENT.hashchange, () => {
+      void this.loadWidgets();
+    });
+  }
+
+  private async loadWidgets() {
+    const topic = this.getTopicFromUrl();
+
+    if (topic !== undefined) {
+      const widgetArr = await widgetService.getWidgetsByTopicId(topic);
+      if (widgetArr) {
+        const widgetEngine = new WidgetEngine(
+          shuffleArray(widgetArr),
+          this.container,
+        );
+        widgetEngine.startSession();
+      }
+    }
+  }
+
+  private getTopicFromUrl() {
+    const hash = location.hash;
+    const topicPath = hash.split("/")[2];
+
+    return topicPath;
   }
 }
