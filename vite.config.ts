8594184c63b5155de2bc772d89166a38d22962de
Nastya Belova <nastjabelova0@gmail.com>
2026-03-12 15:30:30 +0300

    chore: RST-34 setup vitest, add configs and small test example

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from "vite";
 export default defineConfig({
+  test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    globals: true,
+  },
   server: {
     port: 8080,
     open: true,
