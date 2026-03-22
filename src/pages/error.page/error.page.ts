b1d1c4118d7bdc32a530b313640ae88fea5b6f97
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-03-22 20:13:34 +0300

    refactor: RST-35 Refactor with using constants

--- src/pages/error.page/error.page.ts
+++ src/pages/error.page/error.page.ts
@@ -6,20 +6,30 @@ import { BasePage } from "../base-page.js";
 import image404 from "../../assets/svg/404.svg";
 import "./error.css";
 
+export const CLASS_NAMES_ERROR = {
+  page: "error-page",
+  errorImage: "error__image",
+} as const;
+
+export const STRING_CONSTANTS_ERROR = {
+  errorImageAlt: "Error Image",
+  buttonText: "Go to Home page",
+} as const;
+
 export class ErrorPage extends BasePage {
   create(parent: HTMLElement): void {
     parent.append(this.container);
-    this.container.classList.add("error-page");
+    this.container.classList.add(CLASS_NAMES_ERROR.page);
 
     new ImageCreator({
       parent: this.container,
-      classes: ["error__image"],
+      classes: [CLASS_NAMES_ERROR.errorImage],
       src: image404,
-      alt: "Error Image",
+      alt: STRING_CONSTANTS_ERROR.errorImageAlt,
     });
 
     const button = new ButtonCreator({
-      text: "Go to Home page",
+      text: STRING_CONSTANTS_ERROR.buttonText,
       classes: [CLASS_NAME.button],
       parent: this.container,
     }).getElement();
