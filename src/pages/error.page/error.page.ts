1a740a489455d62735dcba7ac09b076fa0b1be0f
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-03-22 19:52:07 +0300

    feat: RST-35 Add image to error page

--- src/pages/error.page/error.page.ts
+++ src/pages/error.page/error.page.ts
@@ -2,7 +2,10 @@ import { HEADINGS_TWO } from "../../constants.js";
 import { RoutePath } from "../../types/route-path.enum.js";
 import ButtonCreator from "../../utils/button/button-creator.js";
 import HeadingsCreator from "../../utils/headings/headings-creator.js";
+import ImageCreator from "../../utils/image/image-creator.js";
 import { BasePage } from "../base-page.js";
+import image404 from "../../assets/svg/404.svg";
+import "./error.css";
 
 export class ErrorPage extends BasePage {
   create(parent: HTMLElement): void {
@@ -13,6 +16,13 @@ export class ErrorPage extends BasePage {
       parent: this.container,
     }).getElement();
     pageTitle.textContent = "404";
+
+    new ImageCreator({
+      parent: this.container,
+      classes: ["error__image"],
+      src: image404,
+      alt: "Error Image",
+    });
 
     const button = new ButtonCreator({
       text: "To dashboard page",
