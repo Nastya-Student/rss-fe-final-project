f080f64e9e7fe4672b0aa1b8f910bdb4c92d8f42
Ulya <ulya.working@yandex.ru>
2026-03-29 21:41:01 +0300

    feat: RST-17 add logic to widget

--- src/widgets/code-completion.widget/code-completion.widget.ts
+++ src/widgets/code-completion.widget/code-completion.widget.ts
@@ -14,6 +14,7 @@ import ButtonCreator from "../../utils/button/button-creator.js";
 import ElementCreator from "../../utils/element-creator.js";
 import HeadingsCreator from "../../utils/headings/headings-creator.js";
 import ParagraphCreator from "../../utils/paragraph/paragraph-creator.js";
+import InputCreator from "../../utils/input/input-creator.js";
 import "./code-completion.widget.css";
 
 export default function codeCompletionWidget(
@@ -32,15 +33,70 @@ export default function codeCompletionWidget(
     text: "Code Completion Widget",
   }).getElement();
 
+  new ParagraphCreator({
+    parent: codeCompletionWidgetContainer,
+    text: "Complete the code snippets by filling in missing parts",
+  }).getElement();
+
   new HeadingsCreator(HEADINGS_THREE, {
     parent: codeCompletionWidgetContainer,
     text: payload.title,
   }).getElement();
 
-  new ParagraphCreator({
+  const codeField = new ElementCreator({
     parent: codeCompletionWidgetContainer,
-    text: payload.code,
+    classes: ["completion-code-container"],
   }).getElement();
+
+  const codeText = payload.code;
+  const codeTextArray = codeText.split("\n");
+  const blankMark = payload.blanks[0] ?? "____";
+
+  for (const item of codeTextArray) {
+    let currentIndex = 0;
+    let blankIndex = item.indexOf(blankMark, currentIndex);
+
+    if (blankIndex == -1) {
+      codeField.append(item);
+    } else {
+      const complicatedString = new ElementCreator({
+        parent: codeField,
+        classes: ["complicated-string"],
+      }).getElement();
+
+      while (blankIndex !== -1) {
+        const textBefore = item.slice(currentIndex, blankIndex);
+        if (textBefore) {
+          new ElementCreator({
+            tag: "span",
+            text: textBefore,
+            classes: ["code-completion-text"],
+            parent: complicatedString,
+          }).getElement();
+        }
+
+        const innerInput = new InputCreator({
+          placeholder: "",
+          classes: ["code-completion-input"],
+          parent: complicatedString,
+        }).getElement();
+        innerInput.type = "text";
+
+        currentIndex = blankIndex + blankMark.length;
+        blankIndex = item.indexOf(blankMark, currentIndex);
+      }
+
+      const textAfter = item.slice(currentIndex);
+      if (textAfter) {
+        new ElementCreator({
+          tag: "span",
+          text: textAfter,
+          classes: ["code-completion-text"],
+          parent: complicatedString,
+        }).getElement();
+      }
+    }
+  }
 
   const submitButton = new ButtonCreator({
     text: STRING_CONSTANTS_PRACTICE.submit,
@@ -48,10 +104,22 @@ export default function codeCompletionWidget(
     parent: codeCompletionWidgetContainer,
   }).getElement();
 
-  const selectedAnswerIndex: CodeCompletionAnswer = { answer: ["map"] };
+  //const answersArray: CodeCompletionAnswer = { answer: ["map"] };
 
   submitButton.addEventListener(EVENT.click, () => {
-    onAnswer(selectedAnswerIndex);
+    const inputValues = document.querySelectorAll(".code-completion-input");
+
+    const answersArray: CodeCompletionAnswer = { answer: [] };
+
+    for (const item of inputValues) {
+      if (item instanceof HTMLInputElement) {
+        answersArray.answer.push(item.value);
+      }
+    }
+    // const userAnswer = input.value.trim();
+    // let answersArray: CodeCompletionAnswer = { answer: [userAnswer] };
+
+    onAnswer(answersArray);
     submitButton.classList.add(CLASS_NAME.noActive);
     submitButton.disabled = true;
   });
