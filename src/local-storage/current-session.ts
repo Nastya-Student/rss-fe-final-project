5b6a7f91dc0caddc6df1b40c9173a28de4db56ca
Nastya Belova <nastjabelova0@gmail.com>
2026-03-29 22:03:30 +0300

    feat: RST-33 add functions to practice-controller.ts

--- src/local-storage/current-session.ts
+++ src/local-storage/current-session.ts
@@ -7,19 +7,29 @@ export const setSession = (session: PracticeSession) => {
   localStorage.setItem(LOCAL_STORAGE.currentSession, JSON.stringify(session));
 };
 
-export const getSession = (): PracticeSession => {
+export const getSession = (): PracticeSession | undefined => {
   const session: unknown = JSON.parse(
     localStorage.getItem(LOCAL_STORAGE.currentSession) ?? "",
   );
   if (isPracticeSession(session)) {
     return session;
   }
-  throw new Error("please, create a local session first");
+  // throw new Error("please, create a local session first");
+  return undefined;
 };
 
-export const updateSession = (answer: SessionAnswer): void => {
+export const updateSession = (
+  answer: SessionAnswer,
+  score: number,
+  newDate: string,
+): void => {
   const session = getSession();
+  if (session === undefined) {
+    throw new Error("please, create a local session first");
+  }
   session.answers.push(answer);
+  session.score += score;
+  session.completedAt = newDate;
   setSession(session);
 };
 
