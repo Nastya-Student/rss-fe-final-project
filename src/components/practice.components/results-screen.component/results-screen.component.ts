ebb76403dcaa5b43f1bff1e364bfa0f9cac174bd
Nastya Belova <nastjabelova0@gmail.com>
2026-04-03 01:14:57 +0300

    style: RST-33 add styles to result screen

--- src/components/practice.components/results-screen.component/results-screen.component.ts
+++ src/components/practice.components/results-screen.component/results-screen.component.ts
@@ -1,4 +1,5 @@
-import { CLASS_NAME, HEADINGS_TWO } from "../../../constants.js";
+import { CLASS_NAME, HEADINGS_ONE, HEADINGS_TWO } from "../../../constants.js";
+import { PracticeSession } from "../../../interfaces/practice-session.interface.js";
 import { getSession } from "../../../local-storage/current-session.js";
 import { CLASS_NAMES_PRACTICE } from "../../../pages/practice.page/practice.page.js";
 import { RoutePath } from "../../../types/route-path.enum.js";
@@ -7,12 +8,14 @@ import ButtonCreator from "../../../utils/button/button-creator.js";
 import ElementCreator from "../../../utils/element-creator.js";
 import HeadingsCreator from "../../../utils/headings/headings-creator.js";
 import ListItemCreator from "../../../utils/list-item/list-item-creator.js";
-import ParagraphCreator from "../../../utils/paragraph/paragraph-creator.js";
 import UnorderedListCreator from "../../../utils/unordered-list/unordered-list-creator.js";
 import "./results-screen.component.css";
 
-export default function resultsScreenComponent(widgets: Widget[]): HTMLElement {
-  const session = getSession();
+export default function resultsScreenComponent(
+  widgets: Widget[],
+  practiceSession?: PracticeSession,
+): HTMLElement {
+  const session = practiceSession ?? getSession();
   if (session === undefined) {
     throw new Error("please, create a local session first");
   }
@@ -21,15 +24,25 @@ export default function resultsScreenComponent(widgets: Widget[]): HTMLElement {
     classes: [CLASS_NAMES_PRACTICE.resultsScreenContainer],
   }).getElement();
 
-  new ParagraphCreator({
+  const header = new ElementCreator({
+    classes: ["practice__header"],
     parent: resultsScreenContainer,
-    text: "Results Screen",
   }).getElement();
 
+  new ButtonCreator({
+    parent: header,
+  });
+
+  new HeadingsCreator(HEADINGS_ONE, {
+    text: "Awesome Results:",
+    classes: ["practice__title"],
+    parent: header,
+  });
+
   const libraryButton = new ButtonCreator({
-    parent: resultsScreenContainer,
     text: "Go back to Library",
     classes: [CLASS_NAME.button],
+    parent: header,
   }).getElement();
   libraryButton.dataset.route = RoutePath.Library;
 
@@ -52,11 +65,12 @@ export default function resultsScreenComponent(widgets: Widget[]): HTMLElement {
   const answers = session.answers;
   for (const item of answers) {
     const widgetId = item.widgetId;
+
     const widget = widgets.find((item) => item.id === widgetId);
     if (!widget) {
-      return;
+      continue;
     }
-    const question = widget.topicId;
+    const question = widget.payload.title;
     const answer = item.isCorrect;
 
     const answersItem = new ListItemCreator({
@@ -72,7 +86,10 @@ export default function resultsScreenComponent(widgets: Widget[]): HTMLElement {
 
     new ElementCreator({
       text: String(answer),
-      classes: ["result__answer"],
+      classes: [
+        "result__answer",
+        answer ? "result__correct-answer" : "result__incorrect-answer",
+      ],
       parent: answersItem,
     }).getElement();
   }
